64bcefece4813844cffcf42c6c1e2458a9932bd7
Tomi Schumacher <tomi.schumacher@thinkbiganalytics.com>
2016-02-25 12:34:04 -0800

    fixed tests, and code warnings

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,8 @@
-DROP TABLE people IF EXISTS;
+-- this is apache Derby syntax
+--DROP TABLE people;
 
-CREATE TABLE people  (
-    person_id BIGINT IDENTITY NOT NULL PRIMARY KEY,
+CREATE TABLE  people  (
+    person_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
     first_name VARCHAR(20),
     last_name VARCHAR(20)
 );
